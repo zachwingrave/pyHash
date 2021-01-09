5c586c092c2df0aec0cb553bea2ba5625bd3b439
Zach Wingrave <z.wingrave@gmail.com>
2021-01-09 22:16:26 +1100

    Update and refactor. Add tqdm for progress indication. Add SHA family of hash functions. Allow user to select hash function.

--- defend.py
+++
@@ -1,9 +0,0 @@
-from hashlib import md5
-
-class Defender:
-
-  def __init__(self):
-    return
-
-  def hash_md5(self, string):
-    return md5(string.encode("utf-8"))
--- hashing.py
+++ hashing.py
@@ -1,90 +1,63 @@
-from string import ascii_letters
-from string import punctuation
-from string import digits
+from string import ascii_letters, punctuation, digits
+import itertools, tqdm, time, hashlib
 
-from itertools import product
-from time import time, sleep
-from os import system, name
-from hashlib import md5
+ALPHABET = ascii_letters + punctuation + digits # try changing this, what happens?
+FUNCTIONS = ("md5", "sha1", "sha224", "sha256", "sha384", "sha512")
+
 
 def main():
-  running = True
-  while running:
-    clear_screen()
-    print("Hashing Playground v1.0 | MD5")
-    print("=============================")
-    print("1. Single Brute Force")
-    print("2. List Brute Force")
-    print("3. Single Dictionary")
-    print("4. List Dictionary")
-    print("5. Quit")
+  password = input("Enter password: ").encode("utf-8")
+  function = input("Enter hash function: ").strip().lower()
 
-    option = int(input("\nEnter your choice: "))
+  if function not in FUNCTIONS:
+    raise Exception("Unsupported hash function.") # check the FUNCTIONS list
+  else:
+    function = getattr(hashlib, function)
 
-    clear_screen()
-    if option == 1:
-      print("Brute Force Single")
-      print("==================")
-      password = input("Enter password (default = \"hash\"): ")
-      if password == "":
-        single_brute_force_md5()
-      else:
-        single_brute_force_md5(password)
-      input("Press [ENTER] to continue.")
-    elif option == 2:
-      print("Under construction.", end=" ")
-      input("Press [ENTER] to continue.")
-    elif option == 3:
-      print("Under construction.", end=" ")
-      input("Press [ENTER] to continue.")
-    elif option == 4:
-      print("Under construction.", end=" ")
-      input("Press [ENTER] to continue.")
-    elif option == 5:
-      running = False
-    else:
-      print("Invalid option.", end=" ")
-      input("Press [ENTER] to continue.")
-    option = None
+  password = function(password).hexdigest() # password is hashed here
+  results = attack(password, function)
 
-  raise SystemExit
-
-def single_brute_force_md5(password="hash"):
-  alphabet = ascii_letters + punctuation + digits
-  utf_password = password.encode("utf-8")
-  hashed_pass = md5(utf_password).hexdigest()
-  print("MD5 Hash:", hashed_pass)
-  cracked = False
-  attempts = 0
-
-  start_time = time()
-  for length in range(1, 32):
-    print("Hashing passwords of length:", length, end=" ")
-    for word in product(alphabet, repeat=length):
-      attempts += 1
-      guess = "".join(word).encode("utf-8")
-      hashed_guess = md5(guess).hexdigest()
-      if hashed_guess == hashed_pass:
-        end_time = time() - start_time
-        cracked = True
-        break
-    print("... Done.")
-    if cracked:
-      break
-  
-  print_report(cracked, attempts, end_time)
-
-def print_report(cracked, attempts, time):
-  if cracked:
-    print("Cracked!", end=" ")
+  if results["cracked"]:
+    print("Cracked!", end=" ") # printing our results
   else:
     print("Password not found.", end=" ")
-  print("Took", attempts, "attempts and", time, "seconds to compute.")
 
-def clear_screen():
-  if name == "nt":
-    system("cls")   # System is running Windows.
-  else:
-    system("clear") # System is running Unix.
+  print("Took", results["attempts"], "attempts and", results["duration"], "seconds to compute.")
 
-main()
+
+def attack(password, function):   # where the magic happens!
+  print("Target Hash:", password)
+  start_time = time.time()
+
+  results = {
+    "cracked": False,
+    "attempts": 0,
+    "duration": 0,
+  }
+
+  # this is where the hard work happens
+
+  for length in range(0, 16):
+    print("Hashing passwords of length:", length)
+    words = itertools.product(ALPHABET, repeat=length)
+
+    for word in tqdm.tqdm(words): # this will happen for every single combination
+      results["attempts"] += 1
+      word = "".join(word).encode("utf-8")
+      guess = function(word).hexdigest()
+
+      if guess == password:
+        end_time = round(time.time() - start_time, 2)
+        results["duration"] = end_time
+        results["cracked"] = True   # gotcha!
+        return results
+
+  # and the hard work is done!
+
+  end_time = round(time.time() - start_time, 2)
+  results["duration"] = end_time
+  return results
+
+
+if __name__ == "__main__":
+  main() # run the program
